dec15f7b646d8ab63c8b676f975ea52dea8103fb
Akira Yumiyama <yumiyama.akira@gmail.com>
2014-04-28 18:36:23 +0900

    fix solo.rb

--- solo.rb
+++ solo.rb
@@ -1,4 +1,4 @@
 file_cache_path "/tmp/chef-solo/cache"
-cookbook_path ["/tmp/chef-solo/cookbooks", "/tmp/chef-solo/cookbooks2"]
+cookbook_path ["/tmp/chef-solo/cookbooks", "/tmp/chef-solo/site-cookbooks"]
 role_path "/tmp/chef-solo/roles"
 log_level :debug
