c6a06f2a1c06ce3ae360fb1cb2c5a7331f39fc91
Akira Yumiyama <yumiyama.akira@gmail.com>
2014-04-28 19:46:12 +0900

    berks path

--- solo.rb
+++ solo.rb
@@ -1,4 +1,5 @@
 file_cache_path "/tmp/chef-repo/cache"
-cookbook_path ["/tmp/chef-repo/cookbooks", "/tmp/chef-repo/site-cookbooks", "~/.berkshelf/cookbooks"]
+berks_path = File.expand_path("~/.berkshelf/cookbooks")
+cookbook_path ["/tmp/chef-repo/cookbooks", "/tmp/chef-repo/site-cookbooks", berks_path]
 role_path "/tmp/chef-repo/roles"
 log_level :debug
